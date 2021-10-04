3ca3d279f35c0604029eac42eae946e7d3ecd5f3
Klaus Azesberger <kazesberger@blackshark.ai>
2021-10-04 18:35:39 +0200

    Add more experiments

--- cookbook/bsk/minimum_example.py
+++ cookbook/bsk/minimum_example.py
@@ -1,7 +1,24 @@
 # Copyright (C) 2015-2021 Blackshark.ai GmbH. All Rights reserved. www.blackshark.ai
-from typing import List
+from itertools import islice, chain
+from typing import List, Iterable, Any, Generator
 
 from flytekit import LaunchPlan, Resources, dynamic, task, workflow
+from flytekit.core.node import Node
+from flytekit.core.node_creation import create_node
+
+
+def batch(iterable: Iterable[Any], batch_size: int) -> Generator[Iterable[Any], None, None]:
+    """
+    Generator that batches any iterable into chunks of the given size.
+    The last chunk can have a smaller size.
+    """
+    orig_iter = iter(iterable)
+    while True:
+        try:
+            batch_iter = islice(orig_iter, batch_size)
+            yield chain([next(batch_iter)], batch_iter)
+        except StopIteration:
+            return
 
 
 @task(requests=Resources(cpu="1", mem="1Gi"), limits=Resources(cpu="1", mem="1Gi"))
@@ -57,14 +74,49 @@ def dynamic_fan_out_task(input_integers: List[int]) -> None:
         complex_single_integer_subworkflow(some_integer=input_integer)
 
 
+@dynamic
+def dynamic_fan_out_task_with_sequential_dependencies(input_integers: List[int]) -> None:
+    nodes: List[Node] = []
+    for input_integer in input_integers:
+        new_node = create_node(complex_single_integer_subworkflow, some_integer=input_integer)
+        if len(nodes) > 0:
+            nodes[-1].runs_before(new_node)
+        nodes.append(new_node)
+
+
+@dynamic
+def dynamic_fan_out_task_with_some_sequential_dependencies(input_integers: List[int]) -> None:
+    last_nodes: List[Node] = []
+    for input_batch in batch(input_integers, 10):
+        new_nodes = [
+            create_node(complex_single_integer_subworkflow, some_integer=input_integer) for input_integer in input_batch
+        ]
+        if len(last_nodes) > 0:
+            for new_node in new_nodes:
+                last_nodes[-1].runs_before(new_node)
+        last_nodes.append(new_nodes[-1])
+
+
 @workflow
-def flye_large_fan_out_fail(number_of_inputs: int) -> None:
+def large_fan_out_fail(number_of_inputs: int) -> None:
     input_integers = create_input_integers_task(number_of_inputs=number_of_inputs)
     dynamic_fan_out_task(input_integers=input_integers)
 
 
+@workflow
+def large_fan_out_with_sequential_dependencies(number_of_inputs: int) -> None:
+    input_integers = create_input_integers_task(number_of_inputs=number_of_inputs)
+    dynamic_fan_out_task_with_sequential_dependencies(input_integers=input_integers)
+
+
+@workflow
+def large_fan_out_with_some_sequential_dependencies(number_of_inputs: int) -> None:
+    input_integers = create_input_integers_task(number_of_inputs=number_of_inputs)
+    dynamic_fan_out_task_with_some_sequential_dependencies(input_integers=input_integers)
+
+
 LaunchPlan.create(
     "Minimum example with max parallelism",
-    flye_large_fan_out_fail,
+    large_fan_out_fail,
     max_parallelism=100,
 )
